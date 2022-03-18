51b236853ccc812380d7b0b5e603571027ef5351
Jakub Charvát <charvj12@fit.cvut.cz>
2022-03-18 16:37:39 +0100

    Initial Commit

---
+++ Headerify/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Headerify
+//
+//  Created by Jakub Charvát on 18.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
